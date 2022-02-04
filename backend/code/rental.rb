d464879a306cc64a37786b547cd5b561bbddf114
tdutreui <thomas.dutreuilh@gmail.com>
2022-02-04 14:41:28 +0100

    level2 - merged - 25mn + adapt test file to run all levels at once + use an option for different level1/2 calculation

--- backend/code/rental.rb
+++ backend/code/rental.rb
@@ -7,13 +7,43 @@ class Rental
     end
   end
 
-  def compute_price(car)
+  def compute_price(car,opts={})
+    with_per_day_discount=opts[:with_per_day_discount]||false
+
     ndays = (end_date.mjd - start_date.mjd) + 1
-    days_price = ndays * car.price_per_day
+
+    if with_per_day_discount
+      days_price = (1..ndays).map { |i| (1 - Rental.discount_for_nth_day(i)) * car.price_per_day }.sum
+    else
+      days_price = ndays * car.price_per_day
+    end
+
     km_price = distance * car.price_per_km
     {
       "id": id,
       "price": km_price + days_price
     }
+  end
+
+=begin
+  - price per day decreases by 10% after 1 day
+  - price per day decreases by 30% after 4 days
+  - price per day decreases by 50% after 10 days
+=end
+  def self.discount_after_nth_day(n)
+    case n
+    when (10..)
+      0.5
+    when (4..)
+      0.3
+    when (1..)
+      0.1
+    else
+      0
+    end
+  end
+
+  def self.discount_for_nth_day(n)
+    discount_after_nth_day(n - 1)
   end
 end
--- backend/code/test.rb
+++ backend/code/test.rb
@@ -1,21 +1,26 @@
 require 'json'
 
-output_filepath = '../level1/data/output.json'
-expected_output_filepath = '../level1/data/expected_output.json'
 
-Dir.chdir('../level1'){
-  system("ruby", "main.rb")
-}
+levels=[1,2]
+levels.each do |id|
+  output_filepath = "../level#{id}/data/output.json"
+  expected_output_filepath = "../level#{id}/data/expected_output.json"
+  File.delete(output_filepath) if File.exist?(output_filepath)
 
-output=JSON.parse(File.read(output_filepath))
-expected_output=JSON.parse(File.read(expected_output_filepath))
+  Dir.chdir("../level#{id}"){
+    system("ruby", "main.rb")
+  }
 
-if output == expected_output
-  puts "OK"
-else
-  puts "Error"
-  puts "output:"
-  puts output
-  puts "expected:"
-  puts expected_output
+  output=JSON.parse(File.read(output_filepath))
+  expected_output=JSON.parse(File.read(expected_output_filepath))
+
+  if output == expected_output
+    puts "level#{id} - OK"
+  else
+    puts "level#{id} - Error"
+    puts "output:"
+    puts output
+    puts "expected:"
+    puts expected_output
+  end
 end
